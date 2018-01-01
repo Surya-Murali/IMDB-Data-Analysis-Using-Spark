4199c710b9171424452de056c3abab549f3566bc
Surya Murali <msurya25@gmail.com>
2017-12-31 19:00:31 -0500

    Update LRMS.py

--- LRMS.py
+++ LRMS.py
@@ -12,22 +12,22 @@ def parseInput(line):
 	return (int(fields[1]), (float(fields[2]), 1.0))
 
 if __name__ == "__main__":
-conf = SparkConf().setAppName("WorstMovies")
-sc = SparkContext(conf = conf)
+	conf = SparkConf().setAppName("WorstMovies")
+	sc = SparkContext(conf = conf)
 
-lines = sc.textFile("hdfs:///user/maria_dev/ml-100k/u.data")
+	lines = sc.textFile("hdfs:///user/maria_dev/ml-100k/u.data")
 
-movieRatings = lines.map(parseInput)
+	movieRatings = lines.map(parseInput)
 
-ratingTotalsAndCount = movieRatings.reduceByKey(lambda movie1, movie2: ( movie1[0] + movie2[0], movie1[1] + movie2[1] ) )
+	ratingTotalsAndCount = movieRatings.reduceByKey(lambda movie1, movie2: ( movie1[0] + movie2[0], movie1[1] + movie2[1] ) )
 
-averageRatings = ratingTotalsAndCount.mapValues(lambda totalAndCount : totalAndCount[0] / totalAndCount[1])
+	averageRatings = ratingTotalsAndCount.mapValues(lambda totalAndCount : totalAndCount[0] / totalAndCount[1])
 
-sortedMovies = averageRatings.sortBy(lambda x: x[1])
+	sortedMovies = averageRatings.sortBy(lambda x: x[1])
 
-results = sortedMovies.take(10)
+	results = sortedMovies.take(10)
 
-movieNames = loadMovieNames()
+	movieNames = loadMovieNames()
 
-for result in results:
-	print(movieNames[result[0]], result[1])
+	for result in results:
+		print(movieNames[result[0]], result[1])
